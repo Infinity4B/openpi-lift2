3b7b1f88dfc7d125a8a90cd2d7e1cc7231a3bede
Infinity_4B <mail707293768@qq.com>
2026-06-12 11:11:07 +0800

    Add dice task config for LIFT2.

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -1446,6 +1446,25 @@ _CONFIGS = [
       ema_decay=None,
     ) ,
     TrainConfig(
+      name="pi05_dice_100_chunk30_30hz_bs32_nonorm_full_pytorch",
+      model=pi0_config.Pi0Config(
+          pi05=True,
+          action_horizon=30,
+          paligemma_variant="gemma_2b_lora",
+          action_expert_variant="gemma_300m_lora",
+      ),
+      data=LeRobotLift2DataConfig(
+          repo_id="dice_100_30hz_nonorm",
+          default_prompt="Roll the dice and move the small stand the specified number of squares based on the number rolled.",
+          use_quantile_norm=False,
+      ),
+      weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
+      pytorch_weight_path="./checkpoints/pi05_pytorch",
+      num_train_steps=50_000,
+      batch_size=32,
+      ema_decay=None,
+    ) ,
+    TrainConfig(
       name="pi05_better_tube_chunk30_30hz_bs32_nonorm_full_pytorch",
       model=pi0_config.Pi0Config(
           pi05=True,
